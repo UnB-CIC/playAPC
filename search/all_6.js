49fc0391bc4f8d1c3adada8bcf97ad34d708ebfd
Sinayra <sinayra@outlook.com>
2014-05-22 21:42:42 -0300

    Versão reduzida e melhorada da documentação

---
+++ search/all_6.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['pintar',['Pintar',['../group__cor.html#ga7d924fd89974b17e40e2501d1677281b',1,'Pintar(int red, int green, int blue):&#160;playcb.cpp'],['../group__cor.html#ga7d924fd89974b17e40e2501d1677281b',1,'Pintar(int red, int green, int blue):&#160;playcb.cpp']]],
+  ['pintarfundo',['PintarFundo',['../group__cor.html#gac16de8453883bd2f1b6fb245fc81120c',1,'PintarFundo(int red, int green, int blue):&#160;playcb.cpp'],['../group__cor.html#gac16de8453883bd2f1b6fb245fc81120c',1,'PintarFundo(int red, int green, int blue):&#160;playcb.cpp']]],
+  ['playcb_2eh',['playcb.h',['../playcb_8h.html',1,'']]],
+  ['preparaimagem',['PreparaImagem',['../group__cor.html#gacb1d972512ee48acb96bd69717ce810f',1,'PreparaImagem(unsigned char *data, int largura, int altura):&#160;playcb.cpp'],['../group__cor.html#gacb1d972512ee48acb96bd69717ce810f',1,'PreparaImagem(unsigned char *data, int largura, int altura):&#160;playcb.cpp']]]
+];
---
+++ search/functions_1.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['criacirculo',['CriaCirculo',['../group__geo.html#ga86c7cb1123292342569a52c5e0a37212',1,'CriaCirculo(float raio, Ponto meio):&#160;playcb.cpp'],['../group__geo.html#ga86c7cb1123292342569a52c5e0a37212',1,'CriaCirculo(float raio, Ponto meio):&#160;playcb.cpp']]],
+  ['criacircunferencia',['CriaCircunferencia',['../group__geo.html#ga4b06c6a231336f4457c6d320ce706cb1',1,'CriaCircunferencia(float raio, Ponto meio):&#160;playcb.cpp'],['../group__geo.html#ga4b06c6a231336f4457c6d320ce706cb1',1,'CriaCircunferencia(float raio, Ponto meio):&#160;playcb.cpp']]],
+  ['criagrupo',['CriaGrupo',['../group__aux.html#ga1851c071ef66ea3b84f87184408414da',1,'CriaGrupo():&#160;playcb.cpp'],['../group__aux.html#ga1851c071ef66ea3b84f87184408414da',1,'CriaGrupo():&#160;playcb.cpp']]],
+  ['criapoligono',['CriaPoligono',['../group__geo.html#gac04da925af485d989bd8442720a1bf83',1,'CriaPoligono(short int qtd,...):&#160;playcb.cpp'],['../group__geo.html#gac04da925af485d989bd8442720a1bf83',1,'CriaPoligono(short int qtd,...):&#160;playcb.cpp']]],
+  ['criapoligonovetor',['CriaPoligonoVetor',['../group__geo.html#ga7ef40d961dc7c7af36c5e010d6fdb8ea',1,'CriaPoligonoVetor(short int index, Ponto *p):&#160;playcb.cpp'],['../group__geo.html#ga7ef40d961dc7c7af36c5e010d6fdb8ea',1,'CriaPoligonoVetor(short int index, Ponto *p):&#160;playcb.cpp']]],
+  ['criaponto',['CriaPonto',['../group__geo.html#ga24b6f499942109ec11fad45af850eadb',1,'CriaPonto(Ponto p):&#160;playcb.cpp'],['../group__geo.html#ga24b6f499942109ec11fad45af850eadb',1,'CriaPonto(Ponto p):&#160;playcb.cpp']]],
+  ['criaquadrado',['CriaQuadrado',['../group__geo.html#gab052ed7da74a6a08b43d1a8fa653471c',1,'CriaQuadrado(float lado, Ponto cantoesq):&#160;playcb.cpp'],['../group__geo.html#gab052ed7da74a6a08b43d1a8fa653471c',1,'CriaQuadrado(float lado, Ponto cantoesq):&#160;playcb.cpp']]],
+  ['criareta',['CriaReta',['../group__geo.html#ga12eadf83f53b516ebd3fc34ce39b5023',1,'CriaReta(Ponto p1, Ponto p2):&#160;playcb.cpp'],['../group__geo.html#ga12eadf83f53b516ebd3fc34ce39b5023',1,'CriaReta(Ponto p1, Ponto p2):&#160;playcb.cpp']]],
+  ['criaretangulo',['CriaRetangulo',['../group__geo.html#gaf789a21d07353663db5393c6acd61379',1,'CriaRetangulo(float base, float altura, Ponto cantoesq):&#160;playcb.cpp'],['../group__geo.html#gaf789a21d07353663db5393c6acd61379',1,'CriaRetangulo(float base, float altura, Ponto cantoesq):&#160;playcb.cpp']]],
+  ['criatriangulo',['CriaTriangulo',['../group__geo.html#ga7b09b56d9a2baabc5da220abd9087e8c',1,'CriaTriangulo(float base, float altura, Ponto cantoesq):&#160;playcb.cpp'],['../group__geo.html#ga7b09b56d9a2baabc5da220abd9087e8c',1,'CriaTriangulo(float base, float altura, Ponto cantoesq):&#160;playcb.cpp']]]
+];
